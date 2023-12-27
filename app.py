e39662edd5b88d888ea906638f60725ca0d6eb89
npmmew <npmmew20@gmail.com>
2023-12-27 23:43:54 +0300

    Доработка функционала

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+import os
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(database=os.path.join(app.root_path, 'flsite.db')))
 app.config['SECRET_KEY'] = 'esafauihf38i212asefaw3fhi32'
+
+
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'fgaiuhfaiuwehi2u3ohf23iouweof'
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close
+
 
 @app.route('/')
 def home():
@@ -12,32 +36,28 @@ def home():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-
-    if form.validate_on_submit():
-        # Здесь обычно выполняется логика обработки данных формы
-        # Например, регистрация пользователя в базе данных
-        print(f'Username: {form.username.data}')
-        print(f'Email: {form.email.data}')
-        print(f'Password: {form.password.data}')
-
-    return render_template('register.html', form=form)
+    return render_template('/user/register.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-
-    if form.validate_on_submit():
-        print(f'Email: {form.email.data}')
-        print(f'Password: {form.password.data}')
-
     return render_template('login.html', form=form)
 
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+@app.route('/add_order')
+def add_order():
+    return render_template('/user/addorder.html')
 
+
+@app.route('/history_order')
+def history_order():
+    return render_template('/user/history_order.html')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('/manager/mprofile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
